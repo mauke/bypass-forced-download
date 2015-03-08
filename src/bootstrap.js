069207560294a0eb96045c9136aca16ed2f9395f
Lukas Mai <l.mai@web.de>
2015-03-08 11:15:03 +0100

    initial import

---
+++ src/bootstrap.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Components.utils.import('resource://gre/modules/Services.jsm');
+
+const http_on_examine_response = 'http-on-examine-response';
+
+const Ci = Components.interfaces;
+
+const observer = {
+    observe: function (subject, topic, data) {
+        switch (topic) {
+            case http_on_examine_response: {
+                let chan = subject.QueryInterface(Ci.nsIHttpChannel);
+                let cd_old = chan.getResponseHeader('Content-Disposition');
+                let cd_new = cd_old.replace(/^\s*attachment(?![^\s;])/i, 'inline');
+                if (cd_old === cd_new) break;
+                chan.setResponseHeader('Content-Disposition', cd_new, /* merge: */ false);
+                break;
+            }
+        }
+    },
+};
+
+function startup(data, reason) {
+    Services.obs.addObserver(observer, http_on_examine_response, /* weak: */ false);
+}
+
+function shutdown(data, reason) {
+    Services.obs.removeObserver(observer, http_on_examine_response);
+}
+
+function install(data, reason) {
+}
+
+function uninstall(data, reason) {
+}
