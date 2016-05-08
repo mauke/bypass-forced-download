b4e20efc26aff39798c765c92f78e6981f61eebf
Lukas Mai <l.mai@web.de>
2016-05-08 17:22:20 +0200

    implement whitelist

--- bootstrap.js
+++ bootstrap.js
@@ -7,32 +7,37 @@ const http_on_examine_response = 'http-on-examine-response';
 const NS_PREFBRANCH_PREFCHANGE_TOPIC_ID = 'nsPref:changed';
 const MY_PREF_BRANCH = 'extensions.bypass-forced-download.';
 const MY_PREF_BLACKLIST = 'blacklist';
+const MY_PREF_WHITELIST = 'whitelist';
 
 const Ci = Components.interfaces;
 
 const me = {
     branch: null,
 
-    blacklist_pattern: /^(?!)/,
-    update_pattern: function () {
-        const blacklist = this.branch.getCharPref(MY_PREF_BLACKLIST);
-        const words = blacklist.match(/\S+/g) || [];
-        const anychar = '\\w';
+    regex_of_pref: function (pref) {
+        const value = this.branch.getCharPref(pref);
+        const words = value.match(/\S+/g) || [];
+        const anychar     = '[\\w\\-]';
+        const anychar_all = '[\\w\\-.]';
         const words_trans = words.map(
-            function (s)
-                s.replace(/([*?]+)|\W/g,
-                    function (m0, m1)
-                        m1 ? anychar + '{' + (m1.split('?').length - 1) + (m1.indexOf('*') >= 0 ? ',' : '') + '}' :
-                        '\\' + m0
-                )
+            s => s.replace(
+                /((?:\*\*)+)|([*?](?:\?|\*(?!\*))*)|\W/g,
+                (m0, m1, m2) =>
+                    m1 ? anychar_all + '*' :
+                    m2 ? anychar + '{' + (m2.split('?').length - 1) + (m2.indexOf('*') >= 0 ? ',' : '') + '}' :
+                    '\\' + m0
+            )
         );
         const combined = (
             words_trans.length
                 ? '(?:' + words_trans.join('|') + ')'
                 : '(?!)'
         );
-        this.blacklist_pattern = new RegExp('^' + combined + '$', 'i');
+        return new RegExp('^' + combined + '$', 'i');
     },
+
+    blacklist_pattern: /^(?!)/,
+    whitelist_pattern: /^(?!)/,
 
     observe: function (subject, topic, data) {
         switch (topic) {
@@ -44,7 +49,9 @@ const me = {
                     } catch (e) {
                     }
                 }();
-                if (host && this.blacklist_pattern.test(host)) break;
+                if (host && (!this.whitelist_pattern.test(host) || this.blacklist_pattern.test(host))) {
+                    break;
+                }
                 try {
                     const cd_old = chan.getResponseHeader('Content-Disposition');
                     const cd_new = cd_old.replace(/^\s*attachment(?![^\s;])/i, 'inline');
@@ -55,25 +62,26 @@ const me = {
                 break;
             }
 
-            case NS_PREFBRANCH_PREFCHANGE_TOPIC_ID:
+            case NS_PREFBRANCH_PREFCHANGE_TOPIC_ID: {
                 switch (data) {
-                    case MY_PREF_BLACKLIST: {
-                        this.update_pattern();
-                        break;
-                    }
+                    case MY_PREF_BLACKLIST: this.blacklist_pattern = this.regex_of_pref(data); break;
+                    case MY_PREF_WHITELIST: this.whitelist_pattern = this.regex_of_pref(data); break;
                 }
                 break;
+            }
         }
     },
 };
 
 function startup(data, reason) {
     const default_prefs = Services.prefs.getDefaultBranch(MY_PREF_BRANCH);
+    default_prefs.setCharPref(MY_PREF_WHITELIST, '**');
     default_prefs.setCharPref(MY_PREF_BLACKLIST, 'cf-media.sndcdn.com download*.mediafire.com');
 
     me.branch = Services.prefs.getBranch(MY_PREF_BRANCH);
     me.branch.addObserver('', me, /* weak: */ false);
-    me.update_pattern();
+    me.whitelist_pattern = me.regex_of_pref(MY_PREF_WHITELIST);
+    me.blacklist_pattern = me.regex_of_pref(MY_PREF_BLACKLIST);
 
     Services.obs.addObserver(me, http_on_examine_response, /* weak: */ false);
 }
